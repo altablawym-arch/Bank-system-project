fe1e7ef9f0ecc29118ba331d34e7051c312ded16
Tablawy <Tablawy@DESKTOP-32F68R4>
2025-12-11 13:18:22 +0200

    final

--- Bank system project/Admin.h
+++ Bank system project/Admin.h
@@ -11,7 +11,7 @@ public:
     }
 
 
-    void setsalary(double salary) { // عملت سيت للمرتب عشان الادمن يكون له اكسس انه يعرف يعدله 
+    void setsalary(double salary) {  
         if (Validation::vaamount(salary)) {
 
         }
